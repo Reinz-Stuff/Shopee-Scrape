66dc2b18f04cf3a61dc5ef61b81b98108e764d41
Fauzi kurniawan <ozilpym123@gmail.com>
2023-01-24 12:28:29 +0700

    add selenium wire to pass robot detection

--- shopee_api.py
+++ shopee_api.py
@@ -2,57 +2,80 @@ import locale
 import time
 
 import pandas as pd
-from seleniumwire import webdriver  # Import from seleniumwire
+from seleniumwire import webdriver
 from seleniumwire.utils import decode
 import json
 
-# Create a new instance of the Chrome driver
-driver = webdriver.Chrome()
 
-# Go to the Google home page
-url = 'https://shopee.co.id/search?keyword=laptop'
+def scrape_data(page, keyword):
+    driver = webdriver.Chrome()
+    url = f'https://shopee.co.id/search?keyword={keyword}&page={page}'
+    driver.get(url)
+    driver.maximize_window()
 
-driver.get(url)
-driver.maximize_window()
+    time.sleep(2)
 
-# Scroll down
-bottom = False
-a = 0
-while not bottom:
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    driver.execute_script(f"window.scrollTo(0, {a});")
-    if a > new_height:
-        bottom = True
-    a += 5
+    bottom = False
+    a = 0
+    while not bottom:
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        driver.execute_script(f"window.scrollTo(0, {a});")
+        if a > new_height:
+            bottom = True
+        a += 15
 
-# Access requests via the `requests` attribute
-for request in driver.requests:
-    if request.response:
-        if request.url.startswith('https://shopee.co.id/api/v4/search/search_items?by=relevancy&keyword='):
-            response = request.response
-            body = decode(response.body, response.headers.get('Content-Encoding', 'Identity'))
-            decoded_body = body.decode('utf-8')
-            json_data = json.loads(decoded_body)
-            locale.setlocale(locale.LC_ALL, 'id_ID.UTF-8')
-            data = []
+    for request in driver.requests:
+        if request.response:
+            if request.url.startswith('https://shopee.co.id/api/v4/search/search_items?by=relevancy&keyword='):
+                response = request.response
+                body = decode(response.body, response.headers.get('Content-Encoding', 'Identity'))
+                decoded_body = body.decode('utf-8')
+                json_data = json.loads(decoded_body)
+                locale.setlocale(locale.LC_ALL, 'id_ID.UTF-8')
+                data = []
 
-            for i in range(0, len(json_data['items'])):
-                product_name = json_data['items'][i]['item_basic']['name']
-                price        = int(json_data['items'][i]['item_basic']['price'])
-                price        = locale.format_string("%d", (price/100000), grouping=True)
-                rating       = json_data['items'][i]['item_basic']['item_rating']['rating_star']
-                rating       = round(rating, 1)
-                sold         = json_data['items'][i]['item_basic']['historical_sold']
-                location     = json_data['items'][i]['item_basic']['shop_location']
+                for i in range(0, len(json_data['items'])):
+                    product_name = json_data['items'][i]['item_basic']['name']
+                    price = int(json_data['items'][i]['item_basic']['price'])
+                    price = locale.format_string("%d", (price / 100000), grouping=True)
+                    rating = json_data['items'][i]['item_basic']['item_rating']['rating_star']
+                    rating = round(rating, 1)
+                    sold = json_data['items'][i]['item_basic']['historical_sold']
+                    location = json_data['items'][i]['item_basic']['shop_location']
 
-                data_dict = {
-                    'Product': product_name,
-                    'Price(Rp)': price,
-                    'Rating': rating,
-                    'Total sold': sold,
-                    'Location': location,
-                }
-                data.append(data_dict)
+                    data_dict = {
+                        'Product': product_name,
+                        'Price(Rp)': price,
+                        'Rating': rating,
+                        'Total sold': sold,
+                        'Location': location,
+                    }
+                    data.append(data_dict)
 
-            df = pd.DataFrame(data)
-            df.to_excel('result.xlsx', index=False)
+                return data
+
+
+def extract_data(data):
+    df = pd.DataFrame(data)
+
+    return df
+
+
+def run():
+    search = input('Product search keyword: ')
+
+    datas = []
+    for i in range(0, 60):
+        print(f'scraping page...{i+1}')
+        scraping = scrape_data(i, search)
+        datas.extend(scraping)
+        time.sleep(1)
+
+    print('writing file...')
+    extarction = extract_data(datas)
+    extarction.to_excel('result.xlsx', index=False)
+    print('Finished!')
+
+
+if __name__ == '__main__':
+    run()
